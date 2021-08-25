2ac26e4790eba805273cf5060d429d55a679f812
Divya Jomi <divyajomi01@gmail.com>
2021-08-25 12:05:11 +0530

    Added Task model

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2021_08_25_045828) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "title"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
